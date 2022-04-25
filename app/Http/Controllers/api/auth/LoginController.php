98f860c0331f81aaba2ad036794593c218094010
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2022-04-26 04:44:58 +0700

    adding generate token

--- app/Http/Controllers/api/auth/LoginController.php
+++ app/Http/Controllers/api/auth/LoginController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers\api\auth;
 
+use App\Actions\SendResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    public function generateToken()
+    public function generateToken(LoginRequest $request)
     {
+        $validated = $request->validated();
 
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Email atau password salah'],
+            ]);
+        }
+
+        $token = $user->createToken($validated['device_name'])->plainTextToken;
+        $data = [
+            'token' => $token
+        ];
+
+        return SendResponse::handle($data, 'Login berhasil');
     }
 
     public function show()

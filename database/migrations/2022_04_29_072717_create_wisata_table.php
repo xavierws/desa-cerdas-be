34a99ee4dd837f60ba5ed054958a0fa05a172fca
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2023-02-04 17:02:15 +0700

    Update 2022_04_29_072717_create_wisata_table.php

--- database/migrations/2022_04_29_072717_create_wisata_table.php
+++ database/migrations/2022_04_29_072717_create_wisata_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Wisata extends Migration
+class CreateWisataTable extends Migration
 {
     /**
      * Run the migrations.

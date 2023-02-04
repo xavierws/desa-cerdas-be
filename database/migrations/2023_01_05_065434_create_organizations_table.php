3ce15f604eac86a231c90a23f2d47a4a80a68128
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2023-02-04 17:04:16 +0700

    Update 2023_01_05_065434_create_organizations_table.php

--- database/migrations/2023_01_05_065434_create_organizations_table.php
+++ database/migrations/2023_01_05_065434_create_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrganizationTable extends Migration
+class CreateOrganizationsTable extends Migration
 {
     /**
      * Run the migrations.

ddd66c5c156132c877bb84db1096e5d3695872bd
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2023-02-07 12:18:01 +0700

    Update BudgetController.php

--- app/Http/Controllers/api/profile/BudgetController.php
+++ app/Http/Controllers/api/profile/BudgetController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\api\profile;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        
+    }
 }
